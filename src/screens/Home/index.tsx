4889271ee4acdb3d5a7e83b5ee9f4ea7bd51d4ec
Natanael Araújo <natanael@hotmail.co.jp>
2022-05-01 19:57:22 -0300

    Show data saved with AsyncStorage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,7 +30,15 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+
+    const response = await AsyncStorage.getItem(dataKey);
+
+    if (response) {
+      const parsedData = JSON.parse(response);
+
+      setSearchListData(parsedData);
+      setData(parsedData);
+    }
   }
 
   function handleFilterLoginData() {
